3a2a5ff274bd3de88f2dfa602b59d72a5e7f733e
mohamadm890 <94972216+mohamadm890@users.noreply.github.com>
2026-05-03 16:48:53 +0100

    Create plans.tsx

---
+++ src/lib/plans/plans.tsx
@@ -0,0 +1,25 @@
+export const PLANS_FULL = {
+    starter: {
+      name: "Starter",
+      price: "5.94",
+      credits: 40,
+      benefits: ["40 thumbnails", "Standard speed"],
+      badge: null,
+    },
+  
+    creator: {
+      name: "Creator",
+      price: "12.94",
+      credits: 100,
+      benefits: ["100 thumbnails", "Faster generation"],
+      badge: "Most Popular",
+    },
+  
+    pro: {
+      name: "Pro",
+      price: "24.94",
+      credits: 200,
+      benefits: ["200 thumbnails", "Fastest generation", "Priority support"],
+      badge: "Pro",
+    },
+  }
