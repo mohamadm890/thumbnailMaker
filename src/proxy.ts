5b375d43fd852161741855066fae74aed4bb644b
mohamadm890 <94972216+mohamadm890@users.noreply.github.com>
2026-05-03 06:49:42 +0100

    Refactor public route matcher and add logging
    Updated the public route matcher to change the order of routes and added a console log for debugging.

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,10 +2,12 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 
 
-const isPublicRoute = createRouteMatcher(['/','/sign-in(.*)', '/sign-up(.*)', '/pricing(.*)', '/api/webhooks(.*)', '/api/products(.*)',  '/privacy',
-  '/terms', '/contact', '/about'])
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/pricing(.*)', '/api/webhooks(.*)', '/api/products(.*)',  '/privacy',
+  '/terms', '/contact', '/about', '/'])
 
 export default clerkMiddleware(async (auth, req) => {
+  console.log("Is Public Route?", isPublicRoute(req), "Path:", req.nextUrl.pathname);
+  
   if (!isPublicRoute(req)) {
     await auth.protect()
   }
