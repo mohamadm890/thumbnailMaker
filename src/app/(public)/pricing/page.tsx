ad981b076cb09d5a8c97ece7d62b0b47c73ad96f
mohamadm890 <94972216+mohamadm890@users.noreply.github.com>
2026-05-03 17:03:54 +0100

    Refactor PricingPage to use local plans data
    Removed API fetch and replaced with local plans data.

--- src/app/(public)/pricing/page.tsx
+++ src/app/(public)/pricing/page.tsx
@@ -1,16 +1,13 @@
+import { PLANS_FULL } from "@/lib/plans/plans";
 import PricingClient from "./PricingClient";
+
 
 export default async function PricingPage() {
 
+  const ProducatsPlans = PLANS_FULL;
+  const plans = Object.values(ProducatsPlans);
+
   
-  const res = await fetch("https://3000-firebase-ai-thumbnail-1776365387174.cluster-64pjnskmlbaxowh5lzq6i7v4ra.cloudworkstations.dev/api/products", {
-    cache: "no-store",
-    
-  });
-
-  console.log("res", res);
-
-  const data = await res.json();
-  console.log("data", data)
-  return <PricingClient plans={data.items || []} />;
+  
+  return <PricingClient plans={plans || []} />;
 }
