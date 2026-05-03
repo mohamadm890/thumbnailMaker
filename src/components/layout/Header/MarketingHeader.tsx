88c8e696458068e70e3840625a00d88577f6fbeb
mohamadm890 <94972216+mohamadm890@users.noreply.github.com>
2026-05-03 09:17:10 +0100

    Simplify MarketingHeader styling
    Removed background color and blur effect from MarketingHeader.

--- src/components/layout/Header/MarketingHeader.tsx
+++ src/components/layout/Header/MarketingHeader.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function MarketingHeader() {
   return (
-    <header className="w-full  dark:border-zinc-800 bg-white/80 dark:bg-zinc-950/80 backdrop-blur sticky top-0 z-50">
+    <header className="w-full   sticky top-0 z-50">
       
       <div className="max-w-6xl mx-auto px-6 py-4 flex items-center justify-between">
 
