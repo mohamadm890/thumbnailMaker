b757af3116740894912d3adc4a2260e54db56350
mohamadm890 <94972216+mohamadm890@users.noreply.github.com>
2026-05-29 08:09:21 +0100

    Update PricingGrid.jsx

--- src/components/pricing/PricingGrid.jsx
+++ src/components/pricing/PricingGrid.jsx
@@ -2,10 +2,16 @@
 import PricingCard from "./PricingCard";
 
 export default function PricingGrid({ plans, onBuy }) {
-    const activePlans = plans.filter((item) => !item.is_archived);
+
+  console.log("plans", plans);
+
+  const plansArray = Object.entries(plans).map(([id, plan]) => ({
+    id,
+    ...plan,
+  }));
   return (
     <div className="grid md:grid-cols-3 gap-6 max-w-5xl mx-auto">
-      {activePlans.map(plan => (
+      {plansArray?.map(plan => (
         <PricingCard
           key={plan.id}
           plan={plan}
