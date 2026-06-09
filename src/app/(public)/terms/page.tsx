b39c0654b4ab4c5f3627861f1d74039e747353b5
mohamadm890 <94972216+mohamadm890@users.noreply.github.com>
2026-06-09 11:43:16 +0100

    Add robots metadata to TermsPage
    Add metadata for robots to control indexing and following.

--- src/app/(public)/terms/page.tsx
+++ src/app/(public)/terms/page.tsx
@@ -1,5 +1,11 @@
 import { colors } from "@/components/design-system/colors";
 
+export const metadata = {
+  robots: {
+    index: false,
+    follow: true,
+  },
+};
 export default function TermsPage() {
   return (
     <div
