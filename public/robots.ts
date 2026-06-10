fe9f2796bc4957e90954928250643d90e8bea94e
mohamadm890 <94972216+mohamadm890@users.noreply.github.com>
2026-06-10 20:41:55 +0100

    Update robots.ts

--- public/robots.ts
+++ public/robots.ts
@@ -1,11 +1,29 @@
 import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
+  const baseUrl = "https://vizthumb.com";
+
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
-    sitemap: "https://vizthumb.com/sitemap.xml",
+    rules: [
+      {
+        userAgent: "*",
+        allow: [
+          "/",
+          "/about",
+          "/pricing",
+
+        ],
+        disallow: [
+          "/generate",
+          "/api/",
+          "/dashboard",
+          "/settings",
+           "/sign-in",
+          "/sign-up",     
+        ],
+      },
+    ],
+
+    sitemap: `${baseUrl}/sitemap.xml`,
   };
 }
