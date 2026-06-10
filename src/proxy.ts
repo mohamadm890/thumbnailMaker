e49796066b76d7440b7f4657225e5d3247723816
mohamadm890 <94972216+mohamadm890@users.noreply.github.com>
2026-06-10 20:11:19 +0100

    Update proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,23 +1,25 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-
-
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/pricing', '/api/webhooks(.*)', '/api/products(.*)',  '/privacy',
-  '/terms', '/contact', '/about', '/', '/refund'])
+const isPublicRoute = createRouteMatcher([
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/pricing",
+  "/privacy",
+  "/terms",
+  "/contact",
+  "/about",
+  "/refund",
+  "/",
+  "/api/webhooks(.*)",
+  "/api/products(.*)",
+]);
 
 export default clerkMiddleware(async (auth, req) => {
-  const path = req.nextUrl.pathname;
-
-  if (path === "/") return;
-
   if (!isPublicRoute(req)) {
-    await auth.protect()
+    await auth.protect();
   }
-})
+});
+
 export const config = {
-  matcher: [
-    '/((?!_next|.*\\..*).*)',
-    '/(api|trpc)(.*)',
-    
-  ],
-}
+  matcher: ["/((?!_next|.*\\..*).*)"],
+};
