4ce0ce357cf58a75476a967d331aed18ebd0e554
mohamadm890 <94972216+mohamadm890@users.noreply.github.com>
2026-06-11 08:29:43 +0100

    Update proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -5,9 +5,9 @@ const isProtectedRoute = createRouteMatcher([
   "/generate(.*)",
 ]);
 
-export default clerkMiddleware((auth, req) => {
+export default clerkMiddleware( async (auth, req) => {
   if (isProtectedRoute(req)) {
-    auth.protect(); 
+   await auth.protect(); 
   }
 });
 
