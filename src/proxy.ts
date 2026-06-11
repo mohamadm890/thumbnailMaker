dbc18f11b7c3bd8a6da1b0f1b543d249cea58a80
mohamadm890 <94972216+mohamadm890@users.noreply.github.com>
2026-06-11 07:34:27 +0100

    Update proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,25 +1,16 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher([
-  "/",
-  "/sign-in(.*)",
-  "/sign-up(.*)",
-  "/terms",
-  "/privacy",
-  "/pricing",
-  "/about",
-  "/contact",
-  "/refund",
-  "/api/webhooks(.*)",
-  "/api/products(.*)",
+const isProtectedRoute = createRouteMatcher([
+  "/dashboard(.*)",
+  "/generate(.*)",
 ]);
 
 export default clerkMiddleware((auth, req) => {
-  if (!isPublicRoute(req)) {
-    auth().protect();
+  if (isProtectedRoute(req)) {
+    auth.protect(); 
   }
 });
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico|.*\\..*).*)"],
+  matcher: ["/dashboard(.*)", "/generate(.*)"],
 };
